97e6bed2f61b7e94b0343e398579417e1bcd5102
Peter Krauss <peter.krauss@upday.com>
2019-01-02 17:53:04 +0100

    add question form and set up firestore

--- src/Question.js
+++ src/Question.js
@@ -1,61 +1,103 @@
-import React from 'react';
-import firebase from "./firebase";
+import React from "react";
+import firebase from "./config/Firebase";
+import FormControl from "@material-ui/core/FormControl";
+import ControlLabel from "@material-ui/core/FormControl";
+import FormGroup from "@material-ui/core/FormGroup";
 
 class Question extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-         choices: "",
-         correctAnswers: "",
-         difficulty: -1,
-         images: "",
-         questions: ""
-        };
-      }
+  constructor() {
+    super();
+    this.state = {
+      choices: "",
+      correctAnswers: "",
+      difficulty: -1,
+      images: "",
+      questions: ""
+    };
+  }
 
-      updateInput = e => {
-        this.setState({
-          [e.target.name]: e.target.value
-        });
-      }
+  updateInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  addQuestion = e => {
+    e.preventDefault();
+    const db = firebase.firestore();
+    db.settings({
+      timestampsInSnapshots: true
+    });
+    const userRef = db.collection("questions").add({
+      choices: this.state.choices,
+      correctAnswers: this.state.correctAnswers,
+      difficulty: this.state.difficulty,
+      images: this.state.images,
+      questions: this.state.questions
+    });
+    this.setState({
+      choices: "",
+      correctAnswers: "",
+      difficulty: 0,
+      images: "",
+      questions: ""
+    });
+  };
 
   render() {
     return (
-        <form>
-          <input
-            type="text"
-            name="choices"
-            placeholder="Choices"
-            onChange={this.updateInput}
-          />
-          <input
-            type="text"
-            name="correctAnswers"
-            placeholder="corrext answer"
-            onChange={this.updateInput}
-          />
-          <input
-            type="number"
-            name="difficulty"
-            placeholder="difficulty"
-            onChange={this.updateInput}
-          />
-          <input
-            type="text"
-            name="images"
-            placeholder="images"
-            onChange={this.updateInput}
-          />
-          <input
-            type="text"
-            name="questions"
-            placeholder="Enter the question"
-            onChange={this.updateInput}
-          />
+      <form horizontal onSubmit={this.addQuestion}>
+        <FormGroup controllId="formbasicText">
+          <ControlLabel>
+            Choices:
+            <input
+              type="text"
+              name="choices"
+              placeholder="Choices"
+              onChange={this.updateInput}
+            />
+          </ControlLabel>
+          <ControlLabel>
+            Correct Answers:
+            <input
+              type="text"
+              name="correctAnswers"
+              placeholder="correct answer"
+              onChange={this.updateInput}
+            />
+          </ControlLabel>
+          <ControlLabel>
+            Difficulty:
+            <input
+              type="number"
+              name="difficulty"
+              placeholder="difficulty"
+              onChange={this.updateInput}
+            />
+          </ControlLabel>
+          <ControlLabel>
+            Images:
+            <input
+              type="text"
+              name="images"
+              placeholder="images"
+              onChange={this.updateInput}
+            />
+          </ControlLabel>
+          <ControlLabel>
+            Questions:
+            <input
+              type="text"
+              name="questions"
+              placeholder="Enter the question"
+              onChange={this.updateInput}
+            />
+          </ControlLabel>
           <button type="submit">Submit</button>
-        </form>
-        );
-      }
-   }
+        </FormGroup>
+      </form>
+    );
+  }
+}
 
 export default Question;
